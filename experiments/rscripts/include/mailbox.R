f0277ad7b9d72831e4493430815e11df89a8ac42
João Vicente Souto <joaovicentesouto@gmail.com>
2019-10-31 21:46:03 -0300

    [exps] Mailbox temporary

---
+++ experiments/rscripts/include/mailbox.R
@@ -0,0 +1,66 @@
+
+#===============================================================================
+# Benchmark Information
+#===============================================================================
+
+if (!exists(script.variant)) {
+	script.variant <- "essential"
+}
+
+benchmark.id                <- "mailbox"
+benchmark.name              <- "Mailbox Benchmark"
+benchmark.dataset.directory <- "./cooked"
+benchmark.dataset.file      <- paste0(paste0("mailbox-", script.variant), ".csv")
+
+#-------------------------------------------------------------------------------
+# Benchmark Parameters
+#-------------------------------------------------------------------------------
+
+# benchmark.params.nthreads.min  <- 1
+# benchmark.params.nthreads.max  <- 14
+# benchmark.params.nthreads.step <- 1
+
+#-------------------------------------------------------------------------------
+# Benchmark Variables
+#-------------------------------------------------------------------------------
+
+benchmark.variables.id   <- c("ncclusters", "kernel")
+benchmark.variables.name <- c("Number of Slaves", "Routine")
+
+#===============================================================================
+# Setup
+#===============================================================================
+
+infile <- paste(
+	benchmark.dataset.directory,
+	benchmark.dataset.file,
+	sep = "/"
+)
+
+data.raw <- read.table(file = infile, sep = ";", header = TRUE)
+
+#==============================================================================
+# Pre-Processing
+#==============================================================================
+
+data.raw$buffersize  <- data.raw$buffersize / KB
+data.raw$latency     <- data.raw$latency / MPPA.FREQ
+data.raw$throughput  <- (data.raw$volume / data.raw$latency) / MB
+
+#===============================================================================
+# Pre-Processing
+#===============================================================================
+
+# Removes unnecessary columns
+data.filtered <- select(data.raw, -c(abstraction,nioclusters,buffersize,latency,volume))
+
+data.melted <- melt(data = data.filtered, id.vars = benchmark.variables.id)
+
+data.digested <- ddply(
+	data.melted,
+	c(benchmark.variables.id, "variable"),
+	summarise,
+	mean = mean(value),
+	sd = sd(value),
+	cv = sd(value)/mean(value)
+)
